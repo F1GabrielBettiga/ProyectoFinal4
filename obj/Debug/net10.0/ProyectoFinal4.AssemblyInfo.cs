99c5322c49d1d293c80c6a8071f3ca541bf1a046
GabrielBettiga <bettgas468@gmail.com>
2026-01-26 17:06:36 -0300

    Creación inicial del proyect
    Se crea la estructura base del proyecto.

--- obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9e3ed815ec8a4977f8b5672a2c918b8d69987ec")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
