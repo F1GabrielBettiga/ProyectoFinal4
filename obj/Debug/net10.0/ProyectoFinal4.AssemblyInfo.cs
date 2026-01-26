fef81814fb1cf90f5057672b6247bf17aa6c322c
GabrielBettiga <bettgas468@gmail.com>
2026-01-26 20:31:43 -0300

    CRUD con controladores MVC (EF Core) para Películas, Géneros y Plataformas
    Se generaron los controllers con Entity Framework para Pelicula, Genero y Plataforma, incluyendo las acciones y vistas completas de Index, Details, Create, Edit y Delete (scaffolding MVC).

--- obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+91ea45400ca1018a6624624e5063e2d7a62e2957")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+516681ff3766e154d9d83edb7c35d557af7b283f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
