c9e3ed815ec8a4977f8b5672a2c918b8d69987ec
GabrielBettiga <bettgas468@gmail.com>
2026-01-26 17:04:35 -0300

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProyectoFinal4.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
