8350fa5009c96f09034a5f5f9e7979d3001e752a
GabrielBettiga <bettgas468@gmail.com>
2026-01-29 17:23:57 -0300

    Implementación de pantalla de detalle de películas (Usuario)
    Se implementó la pantalla de detalle de películas orientada a usuarios comunes.
    La vista muestra la información completa de la película (poster, género, duración, sinopsis, plataforma y reseñas destacadas) y reemplaza las acciones administrativas por un botón para agregar reseña.
    
     La funcionalidad de agregar reseña aún no está implementada, queda pendiente para una próxima iteración.

--- obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
+++ obj/Debug/net10.0/ProyectoFinal4.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+59a03245d12feffe4df477a846c5b0d14959b7ea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e186af465b25072d83cff07212f411c9c74058e6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ProyectoFinal4")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
